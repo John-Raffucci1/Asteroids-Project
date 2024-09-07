ada49b957461a066f664ecfdb474566767f24bea
John Raffucci <john.raffucci1@gmail.com>
2024-09-06 18:52:50 -0700

    Added FPS and tickrate!

--- main.py
+++ main.py
@@ -3,6 +3,8 @@ from constants import *
 
 def main():
     pygame.init()
+    clock = pygame.time.Clock()
+    dt = 0
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     while True:
         for event in pygame.event.get():
@@ -10,6 +12,8 @@ def main():
                 return
         screen.fill((0,0,0))
         pygame.display.flip()
+        clock.tick(60)
+        dt = clock.get_time() / 1000
     print("Starting asteroids!")
     print(f"Screen width: {SCREEN_WIDTH}")
     print(f"Screen height: {SCREEN_HEIGHT}")
